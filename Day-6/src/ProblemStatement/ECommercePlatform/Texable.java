032e07bd6e7b3b99b621773d9365e782b95ce0b0
Rishi Thakur <rishithakur9131@gmail.com>
2025-01-26 23:08:16 +0530

    Added E Commerce Code

---
+++ Day-6/src/ProblemStatement/ECommercePlatform/Texable.java
@@ -0,0 +1,9 @@
+package ProblemStatement.ECommercePlatform;
+
+//Create an interface Texable
+public interface Texable {
+    //Create a method calculateTax
+    double calculateTax();
+    //Create a method getTaxDetails
+    void getTaxDetails();
+}
