5a31739cc18a4c611081ff192e744c9ffd22c5fc
Rishi Thakur <rishithakur9131@gmail.com>
2025-01-26 22:53:27 +0530

    Added Employee Management

---
+++ Day-6/src/ProblemStatement/EmployeeManagementSystem/Department.java
@@ -0,0 +1,9 @@
+package ProblemStatement.EmployeeManagementSystem;
+
+    //create an interface Department
+    public interface Department {
+        //create an abstract method assignedDepartment
+        void assignedDepartment(String department);
+        //create an abstract method getDepartmentDetails
+        void getDepartmentDetails();
+    }
