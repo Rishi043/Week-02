d5a02804b7c5df7f234b76854b6b52250461c176
Rishi Thakur <rishithakur9131@gmail.com>
2025-01-27 10:01:26 +0530

    Added Ride Hailing Application

---
+++ Day-6/src/ProblemStatement/RideHailingApplication/GPS.java
@@ -0,0 +1,10 @@
+package ProblemStatement.RideHailingApplication;
+
+// Interface GPS
+public interface GPS {
+    // Abstract methods
+    String getCurrentLocation();
+
+    // Abstract methods
+    void updateLocation(String newLocation);
+}
